67977bae57d7ba961e35bf1b37dcc0c13c4d67e5
Eduard Reger <eduard.reger@arvato.com>
2020-06-19 20:33:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# noci-icon
+
+TODO: Some screenshots
+
+A small unity tool to create small generic icons.
+
+## Installation
+
+TODO: via upm branch or simply clone
+
+## Usage
+
+TODO: Either as Editor tool and save as Sprite or runtime with several options 
+
+## References
+
+- Based on the algorithm by [yurkth](https://github.com/yurkth/sprator)
+
+**Unity 2019.3.15f1**
---
+++ noci-icon-unity/Assets/Scripts/NociRend.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace drstc.nociincon
+{
+
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class NociRend : MonoBehaviour
+    {
+        SpriteRenderer rend;
+        void Start()
+        {
+            rend = GetComponent<SpriteRenderer>();
+
+            var factory = new NociFactory();
+
+            rend.sprite = factory.GetSprite();
+        }
+
+    }
+}
